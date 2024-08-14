d6ee4f8d1f9e03fe63880085de0af4115d13fef9
EmirSnmezz <emircan_snmez@outlook.com>
2024-08-14 15:10:40 +0300

    Presentation Layer Added

--- E-Commerce.Application/obj/Debug/net8.0/E-CommerceBackEnd.Application.AssemblyInfo.cs
+++ E-Commerce.Application/obj/Debug/net8.0/E-CommerceBackEnd.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-CommerceBackEnd.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84dc9ee4e096655088513fecea29d2f55d408df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c920d54aedc03e94d83b29039ca4ffd68753d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-CommerceBackEnd.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-CommerceBackEnd.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
