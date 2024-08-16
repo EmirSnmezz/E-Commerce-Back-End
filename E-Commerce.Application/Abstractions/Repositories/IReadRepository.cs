55e060a5ed8bbf8764d8a8086fa6868248de3966
EmirSnmezz <emircan_snmez@outlook.com>
2024-08-16 10:24:28 +0300

    .vs error fixed

--- E-Commerce.Application/Abstractions/Repositories/IReadRepository.cs
+++ E-Commerce.Application/Abstractions/Repositories/IReadRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace E_Commerce.Application.Abstractions.Repositories
 {
     public interface IReadRepository<T>: IRepository<T> where T : class
     {
-
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> condition);
+        T GetById(Guid id);
     }
 }
